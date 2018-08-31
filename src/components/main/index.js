455952d39f300bc00d91534e5f9a2cc6bf961828
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2018-08-31 10:35:55 -0400

    Main set up

---
+++ src/components/main/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+// import './styles';
+
+const Main = () => {
+  return (
+    <div className="container-fluid">
+      <h1>Webhooks example</h1>
+
+      <div className="row">
+        <div className="col-md-12">
+    
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main;
