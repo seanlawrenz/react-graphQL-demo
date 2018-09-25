2f4d7951bc5be99ca1246c9ee0a7028e7c7f5960
Sean Lawrenz <sean.lawrenz@teamdynamix.com>
2018-09-25 13:29:11 -0400

    Card #3344677 Routing navagation init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Main from 'components/main';
+import ErrorComponent from 'components/error';
+import Webhook from 'components/webhook';
 import './App.css';
 
 class App extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <HashRouter>
-        <Main />
+        <div>
+          <Route exact path="/404" component={ErrorComponent} />
+          <Route exact path="/" component={Main} />
+          <Route path="/:_uri" component={Webhook} />
+        </div>
       </HashRouter>
     );
   }
